08af88c8feac6dd1c4f48436fb5e58a28d5d1dcf
Kim-MinBeom <kmb3755@naver.com>
2017-07-18 12:26:57 +0900

    Python Basic Training

---
+++ 김민범/Python Basic Training01.py
@@ -0,0 +1,59 @@
+# Python Basic Training01
+
+# 1번
+a = int(input("숫자입력>"))
+print(a)
+print("%d" % (a ** 3))
+
+# 2번 ???
+'''
+a = 3.14
+print("%d" % (a%    ))
+'''
+
+# 3번
+a = int(input("a 입력"))
+b = int(input("b 입력"))
+
+print("%d" % (a//b))
+
+# 4번
+print("'작은 따옴표가 들어간 string'")
+
+# 5번
+print('"큰 따옴표가 들어간 string"')
+
+# 6번
+family_name = input("성 입력>")
+first_name = input("이름 입력>")
+
+full_name = family_name + first_name
+print(full_name)
+
+# 7번
+'''
+myTuple = (1, 2, 3)
+sum_Tuple = (4)
+myTuple = myTuple + sum_Tuple
+print(myTuple)
+'''
+
+# 8번
+a = int(input("a값 입력>"))
+
+if a < 0 :
+    a = a * -1
+print(a)
+
+# 9번
+
+
+# 10번
+a = int(input("a 입력>"))
+
+if a % 2 :
+    print("홀수")
+else :
+    print("짝수")
+
+# 11번
