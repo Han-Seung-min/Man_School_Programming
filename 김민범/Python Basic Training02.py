c1ddcce86d3dbfa17c1467238635d3339b5ab8c6
Kim-MinBeom <kmb3755@naver.com>
2017-08-27 14:08:08 +0900

    Python Basic Training02

--- 김민범/Python Basic Training02.py
+++ 김민범/Python Basic Training02.py
@@ -21,3 +21,20 @@ menupan = {"짬뽕" : 5000, "짜장면" : 4000, "탕수육" : 12000}
 menu = input("메뉴를 입력하세요>>")
 
 print(menupan[menu])
+
+# 4번
+print("프로그래밍"+"분과")
+
+# 5번
+def convermin(sec) :
+    if sec // 60 == 0 :
+        print("해당 값은 %d초 입니다." % sec)
+    else :
+        print("해당 값은 %d분 %d초 입니다." % ((sec//60), sec % 60 ))
+    
+sec = int(input("초를 입력하세요>"))
+
+convermin(sec)
+
+# 6번
+
